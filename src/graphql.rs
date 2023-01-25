e7174a5bd5aeb7c74f231cbf2b35fb60df9be898
Theo Butler <theodusbutler@gmail.com>
2023-01-25 14:26:36 -0500

    Add graphql::http

--- src/graphql.rs
+++ src/graphql.rs
@@ -10,6 +10,34 @@ use serde::{
 use std::collections::{BTreeMap, HashMap};
 use std::convert::TryInto as _;
 
+pub mod http {
+    use serde::Deserialize;
+
+    #[derive(Deserialize)]
+    pub struct Response<T> {
+        pub data: Option<T>,
+        pub errors: Option<Vec<Error>>,
+    }
+
+    #[derive(Deserialize)]
+    pub struct Error {
+        pub message: String,
+    }
+
+    impl<T> Response<T> {
+        pub fn unpack(self) -> Result<T, String> {
+            self.data.ok_or_else(|| {
+                self.errors
+                    .unwrap_or_default()
+                    .into_iter()
+                    .map(|err| err.message)
+                    .collect::<Vec<String>>()
+                    .join(", ")
+            })
+        }
+    }
+}
+
 // TODO: (Performance) may want to do zero-copy here later.
 pub type StaticValue = q::Value<'static, String>;
 
