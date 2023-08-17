3b2747be778b82a003ca277a52fb10da331aff2f
Theo Butler <theodusbutler@gmail.com>
2023-08-17 10:13:21 -0400

    refactor: replace bytes wrapper with ethers-rs types

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,6 @@ pub mod bytes;
 pub mod epoch_cache;
 #[cfg(feature = "graphql")]
 pub mod graphql;
+pub mod thegraph;
 #[cfg(feature = "url")]
 pub mod url;
-
-pub use anyhow;
-pub use bs58;
-pub use faster_hex;
-pub use serde;
-pub use sha3;
