3b299eca955a6ce34f2fc38fb7a5b1af22326eb7
Lorenzo Delgado <lorenzo@edgeandnode.com>
2023-11-28 13:16:10 +0100

    chore(toolshed): remove deprecated graphql sub-module (#43)

--- toolshed/src/lib.rs
+++ toolshed/src/lib.rs
@@ -1,8 +1,5 @@
 pub mod bytes;
 pub mod epoch_cache;
-#[deprecated(since = "0.2.3", note = "Use `graphql` crate instead")]
-#[cfg(feature = "graphql")]
-pub mod graphql;
 pub mod thegraph;
 #[cfg(feature = "url")]
 pub mod url;
