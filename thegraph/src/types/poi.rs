91c3ea9a14c6f8f287481462cecfe996364f3f7d
Lorenzo Delgado <lorenzo@edgeandnode.com>
2023-12-11 19:24:41 +0100

    Update thegraph/src/types/poi.rs
    Co-authored-by: Theo Butler <theodusbutler@gmail.com>

--- thegraph/src/types/poi.rs
+++ thegraph/src/types/poi.rs
@@ -1,5 +1,5 @@
 //! A Proof of Indexing (POI) a cryptographic proof submitted by indexers to demonstrate that they
-//! have  accurately indexed a subgraph.
+//! have accurately indexed a subgraph.
 //!
 //! The POI is essentially a signature over a message digest that is generated during the indexing
 //! of a subgraph from genesis. Each time a subgraph’s state is updated, so does the message digest.
