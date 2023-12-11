c90935e793eefca516097121db8918f0e0a930e1
Lorenzo Delgado <lorenzo@edgeandnode.com>
2023-12-11 19:25:59 +0100

    feat(thegraph): add proof-of-indexing to the types module (#58)
    Co-authored-by: Theo Butler <theodusbutler@gmail.com>

--- thegraph/src/types.rs
+++ thegraph/src/types.rs
@@ -3,11 +3,13 @@
 pub use attestation::*;
 pub use block_pointer::*;
 pub use deployment_id::*;
+pub use poi::*;
 pub use primitives::*;
 pub use subgraph_id::*;
 
 pub mod attestation;
 pub mod block_pointer;
 pub mod deployment_id;
+pub mod poi;
 mod primitives;
 pub mod subgraph_id;
