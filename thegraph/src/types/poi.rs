262aad63ec1e6b3c43c235fab56398e0ab80184d
Lorenzo Delgado <lorenzo@edgeandnode.com>
2023-12-11 19:07:23 +0100

    feat(thegraph): add proof-of-indexing to the types module

---
+++ thegraph/src/types/poi.rs
@@ -0,0 +1,11 @@
+//! A Proof of Indexing (POI) a cryptographic proof submitted by indexers to demonstrate that they
+//! have  accurately indexed a subgraph.
+//!
+//! The POI is essentially a signature over a message digest that is generated during the indexing
+//! of a subgraph from genesis. Each time a subgraph’s state is updated, so does the message digest.
+
+use super::primitives::B256;
+
+/// A Proof of Indexing (POI) a cryptographic proof submitted by indexers to demonstrate that they
+/// have  accurately indexed a subgraph.
+pub type ProofOfIndexing = B256;
