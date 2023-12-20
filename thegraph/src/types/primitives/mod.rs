652af4b34b59a6fb3ee9ccd27c44b7220666ea9f
Jannis Pohlmann <jannis@edgeandnode.com>
2023-12-20 14:34:50 +0100

    refactor: move decimal and token types into thgraph

--- thegraph/src/types/primitives/mod.rs
+++ thegraph/src/types/primitives/mod.rs
@@ -1,3 +1,6 @@
 //! Re-exported fundamental types
 
+mod decimal;
+
 pub use alloy_primitives::{Address, B256, U256};
+pub use decimal::{InvalidDecimalString, UDecimal18};
--- toolshed/src/lib.rs
+++ toolshed/src/lib.rs
@@ -1,6 +1,5 @@
 pub mod buffer_queue;
 pub mod bytes;
-pub mod decimal;
 pub mod double_buffer;
 pub mod epoch_cache;
 #[deprecated(since = "0.4.0", note = "use `thegraph` crate instead")]
