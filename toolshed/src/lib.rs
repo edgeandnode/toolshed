e2eb1bd1030d980984803c20be2502ee27ca22c3
Lorenzo Delgado <lorenzo@edgeandnode.com>
2024-01-03 11:37:56 +0100

    chore(toolshed): remove thegraph deprecated module

--- toolshed/src/lib.rs
+++ toolshed/src/lib.rs
@@ -2,7 +2,5 @@ pub mod buffer_queue;
 pub mod bytes;
 pub mod double_buffer;
 pub mod epoch_cache;
-#[deprecated(since = "0.4.0", note = "use `thegraph` crate instead")]
-pub mod thegraph;
 #[cfg(feature = "url")]
 pub mod url;
