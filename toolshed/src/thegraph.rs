f2d57930d3c0562636b3e742852ad303a28e7583
Lorenzo Delgado <lorenzo@edgeandnode.com>
2024-01-03 15:15:25 +0100

    chore(toolshed): remove thegraph deprecated module (#72)

--- toolshed/src/thegraph.rs
+++
@@ -1,8 +0,0 @@
-pub use block_pointer::*;
-pub use deployment_id::*;
-pub use subgraph_id::*;
-
-pub mod attestation;
-pub mod block_pointer;
-pub mod deployment_id;
-pub mod subgraph_id;
--- toolshed/src/thegraph/block_pointer.rs
+++
@@ -1,8 +0,0 @@
-use alloy_primitives::{BlockHash, BlockNumber};
-use serde::{Deserialize, Serialize};
-
-#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
-pub struct BlockPointer {
-    pub number: BlockNumber,
-    pub hash: BlockHash,
-}
