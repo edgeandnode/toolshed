4ba67bac969e07e91eafda772607185014a5d4c2
Lorenzo Delgado <lorenzo@edgeandnode.com>
2024-05-30 00:10:41 +0200

    feat(thegraph-core): report subgraph client errors
    Signed-off-by: Lorenzo Delgado <lorenzo@edgeandnode.com>

--- thegraph-core/src/client/subgraph_client.rs
+++ thegraph-core/src/client/subgraph_client.rs
@@ -6,34 +6,82 @@ use serde::de::Deserialize;
 use thegraph_graphql_http::graphql::IntoDocument;
 use thegraph_graphql_http::http::request::IntoRequestParameters;
 use thegraph_graphql_http::http_client::ResponseError;
+use tracing::Instrument;
 use url::Url;
 
 use crate::types::BlockPointer;
 
 use super::queries::{
-    meta::send_subgraph_meta_query,
+    meta::send_bootstrap_meta_query,
     page::{send_subgraph_page_query, BlockHeight, SubgraphPageQueryResponseOpaqueEntry},
     send_subgraph_query,
 };
 
+/// Error message returned by the indexer typically when a reorg happens.
+const SUBGRAPH_REORG_ERROR: &str = "no block with that hash found";
+
+#[derive(Debug, Clone, thiserror::Error)]
+pub enum PaginatedQueryError {
+    /// The bootstrap meta query failed.
+    #[error("bootstrap meta query failed: {0}")]
+    BootstrapMetaQueryFailed(String),
+
+    /// The response was empty.
+    ///
+    /// A page query response should always contain at least the meta query response. If the
+    /// response is empty, it means that the subgraph is not returning any data.
+    #[error("empty response")]
+    EmptyResponse,
+
+    /// A reorg was detected.
+    ///
+    /// The indexer responded with an error message indicating that a reorg was detected.
+    #[error("reorg detected")]
+    ReorgDetected,
+
+    /// An error occurred while sending one of the requests.
+    #[error("request error: {0}")]
+    RequestError(String),
+
+    /// An error occurred while processing the query.
+    ///
+    /// This error contains the error messages returned by the indexer when an error occurred while
+    /// processing one of the page requests.
+    #[error("response error: {0:?}")]
+    ResponseError(Vec<String>),
+
+    /// Response deserialization error.
+    ///
+    /// An error occurred while deserializing the response.
+    #[error("deserialization error: {0}")]
+    DeserializationError(String),
+}
+
 async fn send_paginated_query<T: for<'de> Deserialize<'de>>(
     client: &reqwest::Client,
-    subgraph_url: Url,
+    subgraph_url: &Url,
     query: impl IntoDocument + Clone,
     ticket: Option<&str>,
     page_size: usize,
     mut block_height: BlockHeight,
-) -> Result<(Vec<T>, BlockPointer), String> {
+) -> Result<(Vec<T>, Option<BlockPointer>), PaginatedQueryError> {
     debug_assert_ne!(page_size, 0, "page size must be greater than 0");
+
+    // Block at which the query is executed.
+    let mut block_pointer: Option<BlockPointer> = None;
 
     // The last id of the previous batch.
     let mut last_id: Option<String> = None;
-    // Block at which the query is executed.
-    let mut query_block: Option<BlockPointer> = None;
+
     // Vector to store the results of the paginated query.
     let mut results = Vec::new();
 
     loop {
+        tracing::trace!(
+            last_id = %last_id.as_deref().unwrap_or("none"),
+            "sending page query request"
+        );
+
         let response = send_subgraph_page_query(
             client,
             subgraph_url.clone(),
@@ -43,50 +91,71 @@ async fn send_paginated_query<T: for<'de> Deserialize<'de>>(
             page_size,
             last_id,
         )
-        .await?;
+        .await
+        .map_err(PaginatedQueryError::RequestError)?;
 
-        let data = match response {
+        let resp = match response {
             Ok(data) if !data.results.is_empty() => data,
-            Ok(_) if results.is_empty() => return Err("empty response".into()),
+            Ok(_) if results.is_empty() => return Err(PaginatedQueryError::EmptyResponse),
             Ok(_) => break,
             Err(err) => {
                 return match err {
-                    ResponseError::Empty => Err("empty response".into()),
+                    ResponseError::Empty => Err(PaginatedQueryError::EmptyResponse),
                     ResponseError::Failure { errors } => {
+                        // Check if the error message contains the reorg error message.
+                        if errors
+                            .iter()
+                            .any(|err| err.message.contains(SUBGRAPH_REORG_ERROR))
+                        {
+                            tracing::debug!(errors=?errors, "reorg detected");
+                            return Err(PaginatedQueryError::ReorgDetected);
+                        }
+
                         let errors = errors
                             .into_iter()
                             .map(|err| err.message)
                             .collect::<Vec<String>>();
-
-                        if errors
-                            .iter()
-                            .any(|err| err.contains("no block with that hash found"))
-                        {
-                            return Err("reorg detected".into());
-                        }
-
-                        Err(errors.join(", "))
+                        Err(PaginatedQueryError::ResponseError(errors))
                     }
+                };
+            }
+        };
+
+        last_id = {
+            let raw_data = resp.results.last().unwrap().get();
+            match serde_json::from_str::<SubgraphPageQueryResponseOpaqueEntry>(raw_data).ok() {
+                Some(item) => Some(item.id),
+                None => {
+                    return Err(PaginatedQueryError::DeserializationError(
+                        "failed to extract id for last entry".to_string(),
+                    ))
                 }
             }
         };
 
-        last_id = Some(
-            serde_json::from_str::<SubgraphPageQueryResponseOpaqueEntry>(
-                data.results.last().unwrap().get(),
-            )
-            .map_err(|_| "failed to extract id for last entry".to_string())?
-            .id,
+        tracing::trace!(
+            block_number = %resp.meta.block.number,
+            block_hash = %resp.meta.block.hash,
+            page_items_count = %resp.results.len(),
+            last_item_id = %last_id.as_deref().unwrap_or_default(),
+            "received page query response"
         );
-        block_height = BlockHeight::Hash(data.meta.block.hash);
-        query_block = Some(data.meta.block);
 
-        for entry in data.results {
-            results.push(serde_json::from_str::<T>(entry.get()).map_err(|err| err.to_string())?);
+        block_height = BlockHeight::Hash(resp.meta.block.hash);
+        block_pointer = Some(resp.meta.block);
+
+        // Deserialize the response data and push them to the results vector
+        for entity in resp.results {
+            match serde_json::from_str::<T>(entity.get()) {
+                Ok(data) => results.push(data),
+                Err(err) => {
+                    return Err(PaginatedQueryError::DeserializationError(err.to_string()));
+                }
+            }
         }
     }
 
-    Ok((results, query_block.unwrap()))
+    Ok((results, block_pointer))
 }
 
 /// A client for interacting with a subgraph.
@@ -177,37 +246,61 @@ impl Client {
         &self,
         query: impl IntoDocument + Clone,
         page_size: usize,
-    ) -> Result<Vec<T>, String> {
-        let mut latest_block = self.latest_block();
+    ) -> Result<Vec<T>, PaginatedQueryError> {
+        let _span = tracing::debug_span!(
+            "paginated_query",
+            url = %self.subgraph_url,
+            page_size,
+        )
+        .entered();
 
+        // Send a bootstrap meta query if the latest block number is 0.
+        //
         // Graph-node is rejecting values of `number_gte:0` on subgraphs with a larger `startBlock`.
         // This forces us to request the latest block number from the subgraph before sending the
         // paginated query.
+        let mut latest_block = self.latest_block();
         if latest_block == 0 {
-            let init = send_subgraph_meta_query(
+            tracing::debug!("sending bootstrap meta query");
+            let bootstrap_block = send_bootstrap_meta_query(
                 &self.http_client,
                 self.subgraph_url.clone(),
                 self.auth_token.as_deref(),
             )
-            .await?;
+            .in_current_span()
+            .await
+            .map_err(PaginatedQueryError::BootstrapMetaQueryFailed)?;
+
+            tracing::debug!(
+                block_number = bootstrap_block.meta.block.number,
+                block_hash = %bootstrap_block.meta.block.hash,
+                "received bootstrap meta query response"
+            );
 
             // Update the latest block number
-            latest_block = self.update_latest_block(init.meta.block.number);
+            latest_block = self.update_latest_block(bootstrap_block.meta.block.number);
         }
 
-        // Send the paginated query.
-        let (results, latest_block) = send_paginated_query(
+        // Send the paginated query request
+        tracing::debug!(block_number = %latest_block ,"sending request");
+
+        let (results, block) = send_paginated_query(
             &self.http_client,
-            self.subgraph_url.clone(),
+            &self.subgraph_url,
             query,
             self.auth_token.as_deref(),
             page_size,
             BlockHeight::NumberGte(latest_block),
         )
+        .in_current_span()
         .await?;
 
-        // Update the latest block number.
-        self.update_latest_block(latest_block.number);
+        // Update the latest block number
+        if let Some(block) = block {
+            self.update_latest_block(block.number);
+        }
+
+        tracing::debug!(total_items_count = %results.len(), "received response");
 
         Ok(results)
     }
