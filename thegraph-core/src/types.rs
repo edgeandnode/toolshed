c1459caa92450962a389ac611f247019516af671
Lorenzo Delgado <lorenzo@edgeandnode.com>
2024-06-17 13:34:53 +0200

    feat(thegraph-core): add allocation ID and indexer ID new-types
    Signed-off-by: Lorenzo Delgado <lorenzo@edgeandnode.com>

--- thegraph-core/src/types.rs
+++ thegraph-core/src/types.rs
@@ -1,17 +1,21 @@
 //! The graph network services' foundation types.
 
 // Re-export types in the public API
-pub use alloy_primitives;
+pub use allocation_id::*;
+pub use alloy_primitives::{self, Address};
 pub use alloy_sol_types;
 pub use attestation::*;
 pub use block_pointer::*;
 pub use deployment_id::*;
 pub use ethers_core;
+pub use indexer_id::*;
 pub use poi::*;
 pub use subgraph_id::*;
 
+mod allocation_id;
 pub mod attestation;
 pub mod block_pointer;
 pub mod deployment_id;
+mod indexer_id;
 pub mod poi;
 pub mod subgraph_id;
