90f85b993c44ee561a93f6ff5b20bf3919596471
Lorenzo Delgado <lorenzo@edgeandnode.com>
2024-06-24 16:18:56 +0200

    feat(thegraph-core): add allocation ID and indexer ID new-types (#237)
    Signed-off-by: Lorenzo Delgado <lorenzo@edgeandnode.com>

---
+++ thegraph-core/src/types/allocation_id.rs
@@ -0,0 +1,74 @@
+use alloy_primitives::Address;
+
+/// A unique identifier for an allocation: the allocation's Ethereum address.
+///
+/// This is a "new-type" wrapper around `Address` to provide type safety.
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct AllocationId(Address);
+
+impl AllocationId {
+    /// Return the internal representation.
+    pub fn into_inner(self) -> Address {
+        self.0
+    }
+}
+
+impl std::fmt::Display for AllocationId {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        std::fmt::Display::fmt(&self.0, f)
+    }
+}
+
+impl std::fmt::Debug for AllocationId {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        std::fmt::Display::fmt(&self, f)
+    }
+}
+
+impl From<Address> for AllocationId {
+    fn from(address: Address) -> Self {
+        AllocationId(address)
+    }
+}
+
+impl std::str::FromStr for AllocationId {
+    type Err = <Address as std::str::FromStr>::Err;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let address = std::str::FromStr::from_str(s)?;
+        Ok(AllocationId(address))
+    }
+}
+
+impl AsRef<Address> for AllocationId {
+    fn as_ref(&self) -> &Address {
+        &self.0
+    }
+}
+
+impl std::ops::Deref for AllocationId {
+    type Target = Address;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for AllocationId {
+    fn deserialize<D>(deserializer: D) -> Result<AllocationId, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let address = Address::deserialize(deserializer)?;
+        Ok(AllocationId(address))
+    }
+}
+
+impl serde::Serialize for AllocationId {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.0.serialize(serializer)
+    }
+}
---
+++ thegraph-core/src/types/indexer_id.rs
@@ -0,0 +1,74 @@
+use alloy_primitives::Address;
+
+/// A unique identifier for an indexer: the indexer's Ethereum address.
+///
+/// This is a "new-type" wrapper around `Address` to provide type safety.
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct IndexerId(Address);
+
+impl IndexerId {
+    /// Return the internal representation.
+    pub fn into_inner(self) -> Address {
+        self.0
+    }
+}
+
+impl std::fmt::Display for IndexerId {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        std::fmt::Display::fmt(&self.0, f)
+    }
+}
+
+impl std::fmt::Debug for IndexerId {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        std::fmt::Display::fmt(&self, f)
+    }
+}
+
+impl From<Address> for IndexerId {
+    fn from(address: Address) -> Self {
+        IndexerId(address)
+    }
+}
+
+impl std::str::FromStr for IndexerId {
+    type Err = <Address as std::str::FromStr>::Err;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let address = std::str::FromStr::from_str(s)?;
+        Ok(IndexerId(address))
+    }
+}
+
+impl AsRef<Address> for IndexerId {
+    fn as_ref(&self) -> &Address {
+        &self.0
+    }
+}
+
+impl std::ops::Deref for IndexerId {
+    type Target = Address;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for IndexerId {
+    fn deserialize<D>(deserializer: D) -> Result<IndexerId, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let address = Address::deserialize(deserializer)?;
+        Ok(IndexerId(address))
+    }
+}
+
+impl serde::Serialize for IndexerId {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.0.serialize(serializer)
+    }
+}
