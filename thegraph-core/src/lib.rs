3fa5b3edcfe30bd62ff5538e4e10398475988fd9
Lorenzo Delgado <lorenzo@edgeandnode.com>
2024-11-26 20:32:09 +0100

    feat(thegraph-core): gate the attestation module behind a crate feature (#390)
    Signed-off-by: Lorenzo Delgado <lorenzo@edgeandnode.com>

--- thegraph-core/src/lib.rs
+++ thegraph-core/src/lib.rs
@@ -14,10 +14,12 @@
 // Re-export `alloy` crate
 pub use alloy;
 
+#[cfg(feature = "attestation")]
+#[cfg_attr(feature = "attestation", doc(hidden))]
+pub use self::attestation::Attestation;
 #[doc(inline)]
 pub use self::{
     allocation_id::AllocationId,
-    attestation::Attestation,
     block::BlockPointer,
     deployment_id::{DeploymentId, ParseDeploymentIdError},
     indexer_id::IndexerId,
@@ -26,6 +28,8 @@ pub use self::{
 };
 
 mod allocation_id;
+#[cfg(feature = "attestation")]
+#[cfg_attr(docsrs, doc(cfg(feature = "attestation")))]
 pub mod attestation;
 mod block;
 #[deprecated(
