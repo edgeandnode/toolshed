b88983c9f7a1e947d62b3d2114e058e3fbf2efde
Lorenzo Delgado <lorenzo@edgeandnode.com>
2024-12-04 20:03:22 +0100

    revert(thegraph-core): add graph-attestation http header (#414)
    This reverts commit 6d0613efee22d290662849c4c26ff6344859f834.

--- thegraph-core/src/headers.rs
+++
@@ -1,8 +0,0 @@
-//! Common HTTP _typed headers_ used across _The Graph_ network services.
-
-#[cfg(feature = "attestation")]
-mod graph_attestation;
-
-#[cfg(feature = "attestation")]
-#[cfg_attr(docsrs, doc(cfg(feature = "attestation"), inline))]
-pub use graph_attestation::GraphAttestation;
